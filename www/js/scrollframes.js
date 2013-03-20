7f73364b2b072d81d38468dee1ce7cea14eb1554
Philo van Kemenade <phillchill@gmail.com>
2013-03-20 01:57:41 +0000

    made FrameScroller and ScrollController JS prototypes

--- www/js/scrollframes.js
+++ www/js/scrollframes.js
@@ -1,11 +1,94 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var framesId = "#scrollframes"
   
-  $(framesId).mousewheel(function(event, delta, deltaX, deltaY) {
-      console.log(delta, deltaX, deltaY); 
-      $(framesId)
-				.fps(deltaY);        
-  });
+  // define FrameScoller class
+  function FrameScroller (targetDiv, no_of_frames) {
+    // construct FrameScroller
+    this.targetDiv = targetDiv;
+    this.no_of_frames = no_of_frames;
+    this.fps = 0;
+    $(this.targetDiv)
+			.sprite({fps: this.fps, no_of_frames: this.no_of_frames})
+    
+    console.log("FrameScroller initiated");
+    
+    // define methods
+    this.getTargetDiv = function () {
+      return this.targetDiv;
+    };
+    
+    this.getFPS = function () {
+      return this.fps;
+    };
+    
+    this.setFPS = function (fps) {
+      this.fps = fps;
+    };
+    
+  };
+  
+  // define ScrollController class
+  function ScrollController (controlDiv) {
+    // construct ScrollController
+    var _self = this;
+    this.controlDiv = controlDiv;
+    this.delta = 0;
+    this.deltaX = 0;
+    this.deltaY = 0;
+    
+    $(this.controlDiv).mousewheel(function(event, delta, deltaX, deltaY) {
+        _self.mousewheelHandler(event, delta, deltaX, deltaY);
+    });
+    
+    // Get and set Delta (X and Y)
+    this.getDelta = function () {
+      return this.delta;
+    };
+    this.setDelta = function (delta) {
+      this.delta = delta;
+    };
+    
+    this.getDeltaX = function () {
+      return this.deltaX;
+    };
+    this.setDeltaX = function (deltaX) {
+      this.deltaX = deltaX;
+    };
+    
+    this.getDeltaY = function () {
+      return this.deltaY;
+    };
+    this.setDeltaY = function (deltaY) {
+      this.deltaY = deltaY;
+    };
+    
+    this.mousewheelHandler = function (event, delta, deltaX, deltaY) {
+      this.setDelta(delta);
+      this.setDeltaX(deltaX);
+      this.setDeltaY(deltaY);
+    };
+     
+  };
+  
+  // init FrameScroller object
+  var myFrameScroller = new FrameScroller("#scrollframes", 10);
+  console.log(myFrameScroller.getTargetDiv());
+  
+  
+  var myScrollController = new ScrollController("#scrollframes");
+  // $("#scrollframes").mousewheel(function(event, delta, deltaX, deltaY) {
+  //       myScrollController.setDelta(delta);
+  //       myScrollController.setDeltaX(deltaX);
+  //       myScrollController.setDeltaY(deltaY);
+
+  //   });
+  
+  var framesId = "#scrollframes";
+  
+  // $(framesId).mousewheel(function(event, delta, deltaX, deltaY) {
+  //     console.log(delta, deltaX, deltaY); 
+  //     $(framesId)
+  //        .fps(deltaY);        
+  // });
   
   (function($) {
 		$(document).ready(function() {
@@ -16,8 +99,8 @@ document.addEventListener("DOMContentLoaded", function () {
 				.isDraggable()
         .activeOnClick()
         .active();               
-		  $('#scrollframes')
-				.sprite({fps: 6, no_of_frames: 10})
+      // $('#scrollframes')
+      //        .sprite({fps: 6, no_of_frames: 10})
         // .activeOnClick()
         // .active();
 			window.actions = {
