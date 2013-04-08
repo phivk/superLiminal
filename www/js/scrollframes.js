49d6781f883024dc7fbd1340306f6022eebc49b1
Philo van Kemenade <phillchill@gmail.com>
2013-04-08 14:46:00 +0100

    added scaleFrames function (not working yet)

--- www/js/scrollframes.js
+++ www/js/scrollframes.js
@@ -1,69 +1,11 @@
 // TODO
-// contact Sebastian re reverse sprite direction
+// * fullscreen
+// * reverse
+// * hi res images
+// * layout progressbar
+
 document.addEventListener("DOMContentLoaded", function () {  
   start();
-  
-  //  (function($) {
-  //  $(document).ready(function() {
-  // 
-  //    $('#bird')
-  //      .sprite({fps: 9, no_of_frames: 3})
-  //         .spRandom({top: 50, bottom: 200, left: 300, right: 320})
-  //      .isDraggable()
-  //         .activeOnClick()
-  //         .active();               
-  //       // $('#scrollframes')
-  //       //        .sprite({fps: 6, no_of_frames: 10})
-  //         // .activeOnClick()
-  //         // .active();
-  //    window.actions = {
-  //      
-  //      fly_slowly_forwards: function() {
-  //        $('#bird')
-  //          .fps(10)
-  //          .spState(1);
-  //      },
-  //      fly_slowly_backwards: function() {
-  //        $('#bird')
-  //          .fps(10)
-  //          .spState(2);
-  //      },
-  //      fly_quickly_forwards: function() {
-  //        $('#bird')
-  //          .fps(20)
-  //          .spState(1);
-  //      },
-  //      fly_quickly_backwards: function() {
-  //        $('#bird')
-  //          .fps(20)
-  //          .spState(2);
-  //      },
-  //      fly_like_lightning_forwards: function() {
-  //        $('#bird')
-  //          .fps(25)
-  //          .spState(1);
-  //      },
-  //      fly_like_lightning_backwards: function() {
-  //        $('#bird')
-  //          .fps(25)
-  //          .spState(2);
-  //      }
-  //    };
-  // 
-  //    window.page = {
-  //      hide_panels: function() {
-  //        $('.panel').hide(300);
-  //      },
-  //      show_panel: function(el_id) {
-  //        this.hide_panels();
-  //        $(el_id).show(300);
-  //      }
-  //    }
-  // 
-  //  });
-  // })(jQuery);
-	
-	
 }, false);
 
 
@@ -90,9 +32,26 @@ function setupDOM (options) {
 		wrapper.append('<div class="progressDiv"><span></span></div>');
 	}
 	
+	// window.resize handler
+	$(window).resize(function() {
+    console.log('$(window).width(): ', $(window).width());
+    scaleFrames();
+  });
+  
+  // $(window).resize(function() {
+  //   $('body').prepend('<div>' + $(window).width() + '</div>');
+  //   console.log('Handler for .resize() called');
+  // });
+	
 }
 
-
+// reset size of frame div
+function scaleFrames() {
+  // TODO; work like reload on resize
+  $("#scrollframes").css("width", $window.width());
+  $("#scrollframes").css("height", "100%");
+  $("#scrollframes").css("background-size", "1000%, 1000%");
+}
 
 
 // define FrameScoller class
@@ -273,5 +232,3 @@ function ScrollController (controlId, targetFrameScroller) {
   };
    
 };
-
-
